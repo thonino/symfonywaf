b1484da6f53b991980ae61d6a00da5d8d9857762
thonino <thoninoben@gmail.com>
2022-03-30 02:26:35 +0200

    reset

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -21,7 +21,7 @@ class CartController extends AbstractController
         if (array_key_exists($id, $cart)){  $cart[$id]++;}
         else {  $cart[$id] =1; }
         $session->set('cart', $cart);
-        return $this->redirectToRoute('app_booking_new', [ 'id'=>$id,]);
+        return $this->redirectToRoute('cart_show', [ 'id'=>$id,]);
     }
     // Fonction LESS
     #[Route(['en' => '/{lesson}/less','fr' => '/{lesson}/moins',], name: 'cart_less')]
