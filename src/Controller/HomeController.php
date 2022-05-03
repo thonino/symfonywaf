eb470404edfbb162726cea46c9275d9c3c7017ad
thonino <thoninoben@gmail.com>
2022-05-03 19:49:02 +0200

    mag bug factures roles app.user

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,17 +1,18 @@
 <?php
 
 namespace App\Controller;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\LessonRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(LessonRepository $lessonRepo): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'lessons' => $lessonRepo->findAll(),
         ]);
     }
 }
