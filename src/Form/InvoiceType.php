a71b019a7693cb516dfe44623121a428e67d0e24
thonino <thoninoben@gmail.com>
2022-05-10 13:16:42 +0200

    wow label_attr class scroll

--- src/Form/InvoiceType.php
+++ src/Form/InvoiceType.php
@@ -6,17 +6,18 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 class InvoiceType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('lastname',TextType::class,[ 'attr' => ['class' => 'form-control'], 'label' => 'Nom'])
-            ->add('firstname',TextType::class,[ 'attr' => ['class' => 'form-control'], 'label' => 'Prénom'])
-            ->add('phone',TextType::class,[ 'attr' => ['class' => 'form-control'], 'label' => 'Téléphone'])
-            ->add('address',TextType::class,[ 'attr' => ['class' => 'form-control'], 'label' => 'Adresse'])
-            ->add('zipcode',TextType::class,[ 'attr' => ['class' => 'form-control'], 'label' => 'Code postal'])
-            ->add('city',TextType::class,[ 'attr' => ['class' => 'form-control'], 'label' => 'Ville'])
+            ->add('lastname',TextType::class,[ 'attr' => ['class' => 'scroll form-control'],'label_attr' =>['class'=> 'scroll'],'label' => 'Nom',])
+            ->add('firstname',TextType::class,[ 'attr' => ['class' => 'scroll form-control'],'label_attr' =>['class'=> 'scroll'],'label' => 'Prénom'])
+            ->add('phone',TextType::class,[ 'attr' => ['class' => 'scroll form-control'],'label_attr' =>['class'=> 'scroll'], 'label' => 'Téléphone'])
+            ->add('address',TextType::class,[ 'attr' => ['class' => 'scroll form-control'],'label_attr' =>['class'=> 'scroll'], 'label' => 'Adresse'])
+            ->add('zipcode',TextType::class,[ 'attr' => ['class' => 'scroll form-control'], 'label_attr' =>['class'=> 'scroll'],'label' => 'Code postal'])
+            ->add('city',TextType::class,[ 'attr' => ['class' => 'scroll form-control'],'label_attr' =>['class'=> 'scroll'], 'label' => 'Ville'])
         ;
     }
     public function configureOptions(OptionsResolver $resolver): void
