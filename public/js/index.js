68836fb5687a6872fa448ff4c75c17e002e0dc27
thonino <thoninoben@gmail.com>
2022-05-10 12:25:16 +0200

    make scrollrev

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,11 @@
+// alert('tutu');
+// scrollreavel
+const sr2 = ScrollReveal();
+sr2.reveal('.scroll',{duration : 350,origin : 'right',scale : 0.5,interval: 150,});
 // click cookies
 document.querySelector(".cookies").addEventListener("click", () =>{ 
 document.querySelector(".cookies").style.display="none";});
+// Retour achat
+function delayBack() {
+    setTimeout(function(){history.go(-1);}, 1000);
+};
