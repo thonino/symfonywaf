2d8b406dac3ca850cc0f8c5b015349cb5b370786
thonino <thoninoben@gmail.com>
2022-05-22 01:08:21 +0200

    maj form placeholder

--- src/Form/InvoiceType.php
+++ src/Form/InvoiceType.php
@@ -12,12 +12,12 @@ class InvoiceType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('lastname',TextType::class,[ 'attr' => ['class' => 'scroll form-control'],'label_attr' =>['class'=> 'scroll'],'label' => 'Nom',])
-            ->add('firstname',TextType::class,[ 'attr' => ['class' => 'scroll form-control'],'label_attr' =>['class'=> 'scroll'],'label' => 'Prénom'])
-            ->add('phone',TextType::class,[ 'attr' => ['class' => 'scroll form-control'],'label_attr' =>['class'=> 'scroll'], 'label' => 'Téléphone'])
-            ->add('address',TextType::class,[ 'attr' => ['class' => 'scroll form-control'],'label_attr' =>['class'=> 'scroll'], 'label' => 'Adresse'])
-            ->add('zipcode',TextType::class,[ 'attr' => ['class' => 'scroll form-control'], 'label_attr' =>['class'=> 'scroll'],'label' => 'Code postal'])
-            ->add('city',TextType::class,[ 'attr' => ['class' => 'scroll form-control'],'label_attr' =>['class'=> 'scroll'], 'label' => 'Ville'])
+            ->add('lastname',TextType::class,[ 'attr' => ['class' => 'scroll form-control','placeholder' => 'Entrez votre Nom'],'label_attr' =>['class'=> 'scroll'],'label' => 'Nom',])
+            ->add('firstname',TextType::class,[ 'attr' => ['class' => 'scroll form-control','placeholder' => 'Entrez votre Prénom'],'label_attr' =>['class'=> 'scroll'],'label' => 'Prénom'])
+            ->add('phone',TextType::class,[ 'attr' => ['class' => 'scroll form-control','placeholder' => 'Entrez votre Téléphone'],'label_attr' =>['class'=> 'scroll'], 'label' => 'Téléphone'])
+            ->add('city',TextType::class,[ 'attr' => ['class' => 'scroll form-control','placeholder' => 'Entrez votre Ville'],'label_attr' =>['class'=> 'scroll'], 'label' => 'Ville'])
+            ->add('address',TextType::class,[ 'attr' => ['class' => 'scroll form-control','placeholder' => 'Entrez votre adresse'],'label_attr' =>['class'=> 'scroll'], 'label' => 'Adresse'])
+            ->add('zipcode',TextType::class,[ 'attr' => ['class' => 'scroll form-control','placeholder' => '00000'], 'label_attr' =>['class'=> 'scroll'],'label' => 'Code postal (5 chiffres)'])
         ;
     }
     public function configureOptions(OptionsResolver $resolver): void
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -18,18 +18,18 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('firstname', TextType::class,[ 'attr' => ['class' => 'form-control'], 'label' => 'Prénom'])
-        ->add('lastname', TextType::class,[ 'attr' => ['class' => 'form-control'], 'label' => 'Nom'])
-        ->add('email', TextType::class,[ 'attr' => ['class' => 'form-control'],'label' => 'E-mail'])
-        ->add('phone', TextType::class,[ 'attr' => ['class' => 'form-control'],'label' => 'Téléphone'])
-        ->add('city', TextType::class,[ 'attr' => ['class' => 'form-control'],'label' => 'Ville'])
-        ->add('address', TextType::class,[ 'attr' => ['class' => 'form-control'],'label' => 'Adresse'])
-        ->add('zipcode', TextType::class,[ 'attr' => ['class' => 'form-control'],'label' => 'Code postal'])
+        ->add('lastname', TextType::class,[ 'attr' => ['class' => 'form-control','placeholder' => 'Entrez votre Nom'], 'label' => 'Nom'])
+        ->add('firstname', TextType::class,[ 'attr' => ['class' => 'form-control','placeholder' => 'Entrez votre Prénom'], 'label' => 'Prénom'])
+        ->add('email', TextType::class,[ 'attr' => ['class' => 'form-control','placeholder' => 'Entrez votre email'],'label' => 'E-mail'])
+        ->add('phone', TextType::class,[ 'attr' => ['class' => 'form-control','placeholder' => 'Entrez votre Téléphone'],'label' => 'Téléphone'])
+        ->add('city', TextType::class,[ 'attr' => ['class' => 'form-control','placeholder' => 'Entrez votre Ville'],'label' => 'Ville'])
+        ->add('address', TextType::class,[ 'attr' => ['class' => 'form-control','placeholder' => 'Entrez votre adresse'],'label' => 'Adresse'])
+        ->add('zipcode', TextType::class,[ 'attr' => ['class' => 'form-control','placeholder' => '00000'],'label' => 'Code postal (5 chiffres)'])
         ->add(  'RGPDConsent', CheckboxType::class, [
                 'mapped' => false,'constraints' => [ new IsTrue([
                 'message' => 'Tu dois cocher la case accepter.', ]),],
                 'label'=> 'En cochant, j\'accepte les conditions ',])
-        ->add(  'plainPassword', PasswordType::class, ['mapped' => false,'attr' => [
+        ->add(  'plainPassword', PasswordType::class, ['mapped' => false,'attr' => ['placeholder' => 'Entrez votre Mdp',
                 'autocomplete' => 'new-password','class' => 'form-control'],'constraints' => [
                 new NotBlank(['message' => 'Entre ton MDP',]),
                 new Length(['min' => 4,'minMessage' => 'tu es limité à {{ limit }} charactères',
